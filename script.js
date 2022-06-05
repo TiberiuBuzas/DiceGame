6298a4c5deb348d5dbaf39ac3f4bc53ce2e0950f
Tiberiu Buzas <buzastibi310@gmail.com>
2022-06-05 21:54:19 +0200

    Settings Page updated

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const scorePlayer1 = document.getElementById("scorePlayer1");
 const scorePlayer2 = document.getElementById("scorePlayer2");
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
+const inputPlayer1 = document.getElementById("inputPlayer1");
+const inputPlayer2 = document.getElementById("inputPlayer2");
+let settingsMessage = document.getElementById("settingsMessage");
 
 
 let score1 = 0;
@@ -108,25 +111,114 @@ function startGame(){
 function showInfo(){
     gameBoardBlue[0].classList.add("invisible");
     settingsCard[0].classList.add("invisible");
-    settingBtn.classList.add("invisible");
+    //settingBtn.classList.add("invisible");
     infoCard[0].classList.remove("invisible");
 }
 
 function showSettings(){
     gameBoardBlue[0].classList.add("invisible");
-    infoBtn.classList.add("invisible");
+    infoCard[0].classList.add("invisible");
+    //infoBtn.classList.add("invisible");
     settingsCard[0].classList.remove("invisible");
 }
 
 function removeCard(){
-    infoCard[0].classList.add("invisible");
+    infoCard[0].classList.add("invisible")
     settingsCard[0].classList.add("invisible");
     gameBoardBlue[0].classList.remove("invisible");
     settingBtn.classList.remove("invisible"); 
     infoBtn.classList.remove("invisible");
-    
     resetGame();
 }
+
+function saveInputValue1(){
+    let input1 = inputPlayer1.value;
+
+    player1.textContent = input1;
+
+    if(player1.textContent == ""){
+        player1.textContent = "Player1";
+    }
+    if(input1.length < 4 || input1.length > 10 ){
+        showSettingsMessage();
+        player1.textContent = "Player1";
+    }
+
+    centerPlayer1Name();
+    
+}
+
+function saveInputValue2(){
+    let input2 = inputPlayer2.value;
+
+    player2.textContent = input2;
+
+    if(player2.textContent == ""){
+        player2.textContent = "Player2";
+    } 
+    if(input2.length < 4 || input2.length > 10 ){
+        showSettingsMessage();
+        player2.textContent = "Player2";
+    }
+   
+    centerPlayer2Name()
+
+}
+
+
+function showSettingsMessage(){
+    settingsMessage.style.display = "block";
+    inputPlayer1.style.display = "none";
+    inputPlayer2.style.display = "none";
+
+    setTimeout(()=> {
+        settingsMessage.style.display = "none";
+        inputPlayer1.style.display = "block";
+        inputPlayer2.style.display = "block";
+    }, 3000)
+    
+}
+
+
+function centerPlayer1Name(){
+    if(player1.textContent.length=="4"){
+        player1.style.left = "15%";
+    } else if(player1.textContent.length=="5"){
+        player1.style.left = "13%";
+    } else if(player1.textContent.length=="6"){
+        player1.style.left = "11%";
+    } else if(player1.textContent.length=="7"){
+        player1.style.left = "10%";
+    } else if(player1.textContent.length=="8"){
+        player1.style.left = "9%";
+    } else if(player1.textContent.length=="9"){
+        player1.style.left = "7%";
+    } else if(player1.textContent.length=="10"){
+        player1.style.left = "5%";
+    }
+}
+
+function centerPlayer2Name(){
+    if(player2.textContent.length=="4"){
+        player2.style.right = "15%";
+    } else if(player2.textContent.length=="5"){
+        player2.style.right = "13%";
+    } else if(player2.textContent.length=="6"){
+        player2.style.right = "11%";
+    } else if(player2.textContent.length=="7"){
+        player2.style.right = "10%";
+    } else if(player2.textContent.length=="8"){
+        player2.style.right = "9%";
+    } else if(player2.textContent.length=="9"){
+        player2.style.right = "7%";
+    } else if(player2.textContent.length=="10"){
+        player2.style.right = "5%";
+    }
+}
+
+
+inputPlayer1.addEventListener("change", saveInputValue1);
+inputPlayer2.addEventListener("change", saveInputValue2);
 
 startBtn.addEventListener("click", startGame);
 rollBtn.addEventListener("click", rollDice);
